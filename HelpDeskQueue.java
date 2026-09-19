48392e57624b20fd9970d7ec2c95056805b8cafc
galopemach@gmail.com <galopemach@gmail.com>
2026-09-19 15:33:39 +0800

    2nd tentative change

--- HelpDeskQueue.java
+++ HelpDeskQueue.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class HelpDeskQueue {
 	// ---------- Queue ----------
-	private static String[] queue = new String[]{"A", "B", "C", "D", "E"};
+	private static String[] queue = new String[5];
 	private static int front = 0;
 	private static int rear = 0;
 	private static int size = 0;
@@ -29,21 +29,22 @@ public class HelpDeskQueue {
 	}
 
 	static String peek() {
-		if (!isEmpty()) {
-			return "Queue is not empty";
+		if (isEmpty() == true) {
+			return null;
 		}
 
-		return "Queue is empty";
+		return queue[front];
 	}
 
 	static void displayQueue() {
 		String currentQueue = "";
 
-		for(String i: queue) {
-			currentQueue += i;
+		for(int i = 0; i < queue.length; i++) {
+			currentQueue += queue[i];
+			front = (front + 1) % queue.length;
 		}
 
-		System.out.println("Current Order of the Queue " + currentQueue);
+		System.out.println("Current Requests: " + currentQueue);
 	}
 
 	static void displayInfo() {
@@ -52,8 +53,7 @@ public class HelpDeskQueue {
 		}
 
 		for(int i = 0; i < queue.length; i++) {
-			System.out.println("Number " + (i + 1) + ". " + queue[front]);
-
+			System.out.println("No. " + (i + 1) + " in the queue: " + queue[front]);
 			front = (front + 1) % queue.length;
 		}
 	}
@@ -69,33 +69,50 @@ public class HelpDeskQueue {
 		System.out.println("================================");
 
 		while (running) {
-			System.out.println("1. Add Service Request\n" + "2. Process Next Request\n" + "3. View Next Request\n"
-					+ "4. Display Waiting Requests\n" + "5. Display Queue Information\n" + "6. Exit");
+			System.out.println(
+				"1. Add Service Request\n" + 	   
+				"2. Process Next Request\n" + 
+				"3. View Next Request\n" + 
+				"4. Display Waiting Requests\n" + 
+				"5. Display Queue Information\n" + 
+				"6. Exit"
+			);
 
 			System.out.print("Enter Option: ");
 			choice = sc.nextInt();
 			sc.nextLine();
 
-			switch (choice) {
-			case 1:
-				/* enqueue */ break;
-			case 2:
-				/* dequeue */ break;
-			case 3:
-				System.out.println(HelpDeskQueue.peek());
-				break;
-			case 4:
-				HelpDeskQueue.displayQueue();
-				break;
-			case 5:
-				HelpDeskQueue.displayInfo();
-				break;
-			case 6:
-				System.out.println("Exit Program");
-				running = false;
-				break;
-			default:
-				System.out.println("Invalid Input. Try again.");
+			if (HelpDeskQueue.isEmpty() == true) {
+					System.out.println();
+					System.out.println("Queue is empty");
+			} else {
+				switch (choice) {
+				case 1:
+					/* enqueue */ break;
+				case 2:
+					/* dequeue */ break;
+				case 3:
+					System.out.println();
+					System.out.println("Next Request: " + HelpDeskQueue.peek());
+					System.out.println();
+					break;
+				case 4:
+					System.out.println();
+					HelpDeskQueue.displayQueue();
+					System.out.println();
+					break;
+				case 5:
+					System.out.println();
+					HelpDeskQueue.displayInfo();
+					System.out.println();
+					break;
+				case 6:
+					System.out.println("Exit Program" + "\n");
+					running = false;
+					break;
+				default:
+					System.out.println("Invalid Input. Try again.");
+				}
 			}
 		}
 		sc.close();
